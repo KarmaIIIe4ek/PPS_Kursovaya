f0c5dda9487cf9e05f9c5e97e574977e0cd17ee9
Nikita <143674989+KarmaIIIe4ek@users.noreply.github.com>
2025-03-16 17:39:33 +0300

    add group feauters

--- server/routes/user/teacherRouter.js
+++ server/routes/user/teacherRouter.js
@@ -6,8 +6,10 @@ const checkRoleMiddleware = require('../../middleware/checkRoleMiddleware')
 const groupController = require('../../controllers/groupController')
 
 router.post('/group/create', authMiddleware, checkRoleMiddleware('teacher'), groupController.create)
+router.post('/group/deleteById', authMiddleware, checkRoleMiddleware('teacher'), groupController.deleteById)
 router.get('/group/getAllMyGroups', authMiddleware, checkRoleMiddleware('teacher'), groupController.getAllMyGroups)
 router.post('/group/addUserToGroup', authMiddleware, checkRoleMiddleware('teacher'), groupController.addUserToGroup)
-
+router.post('/group/grantRightsToGroup', authMiddleware, checkRoleMiddleware('teacher'), groupController.grantRightsToGroup)
+router.post('/group/changeIsOpenById', authMiddleware, checkRoleMiddleware('teacher'), groupController.changeIsOpenById)
 
 module.exports = router
